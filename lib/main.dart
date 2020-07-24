a35240da7d1aeb54a54849a29bcdee934569e480
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-07-24 14:14:53 +0530

    Added things

--- lib/main.dart
+++ lib/main.dart
@@ -58,8 +58,13 @@ class HomePage extends StatelessWidget {
       drawer: Drawer(
         child: ListView(padding: const EdgeInsets.all(0), children: <Widget>[
           UserAccountsDrawerHeader(
-              accountName: Text("DaCoder555"),
-              accountEmail: Text("nintendo.spy123@gmail.com")),
+            accountName: Text("DaCoder555"),
+            accountEmail: Text("nintendo.spy123@gmail.com"),
+            currentAccountPicture: CircleAvatar(
+              backgroundImage: NetworkImage(
+                  "https://images.unsplash.com/photo-1515879218367-8466d910aaa4?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1050&q=80"),
+            ),
+          ),
           ListTile(
             leading: Icon(Icons.person),
             title: Text("Hi, I am Abhishek"),
