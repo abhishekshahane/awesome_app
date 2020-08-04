c89f9642c722193b73de32c90c8fbc2ed5fa9826
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-04 20:18:54 +0530

    Added complete login

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -21,22 +21,33 @@ class _LoginPageState extends State<LoginPage> {
           child: Form(
         key: formKey,
         // We want this to be a column, not in rows.
-        child: Column(
-          children: <Widget>[
-            TextFormField(
-              controller: _usernameController,
-              keyboardType: TextInputType.emailAddress,
-              decoration: InputDecoration(
-                  hintText: "Enter email", labelText: "Username"),
+        child: Card(
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.all(16.0),
+              child: Column(
+                children: <Widget>[
+                  TextFormField(
+                    controller: _usernameController,
+                    keyboardType: TextInputType.emailAddress,
+                    decoration: InputDecoration(
+                        hintText: "Enter email", labelText: "Username"),
+                  ),
+                  TextFormField(
+                    controller: _passwordController,
+                    keyboardType: TextInputType.text,
+                    obscureText: true,
+                    decoration: InputDecoration(
+                        hintText: "Enter password", labelText: "Password"),
+                  ),
+                  RaisedButton(
+                    onPressed: () {},
+                    child: Text("Enter"),
+                  )
+                ],
+              ),
             ),
-            TextFormField(
-              controller: _passwordController,
-              keyboardType: TextInputType.emailAddress,
-              obscureText: true,
-              decoration: InputDecoration(
-                  hintText: "Enter password", labelText: "Password"),
-            )
-          ],
+          ),
         ),
       )),
     );
