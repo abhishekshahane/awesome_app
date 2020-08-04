e6d0e9879662411a78cd53a6840cb18d2d7f7a89
theabhishekshahane <38487181+abhishekshahane@users.noreply.github.com>
2020-08-04 20:48:31 +0530

    Added picture

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -15,43 +15,56 @@ class _LoginPageState extends State<LoginPage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Login page")),
-      // Lets you scroll up and down your app
-      body: SingleChildScrollView(
-          child: Form(
-        key: formKey,
-        // We want this to be a column, not in rows.
-        child: Card(
-          child: Center(
-            child: Padding(
-              padding: const EdgeInsets.all(16.0),
-              child: Column(
-                children: <Widget>[
-                  TextFormField(
-                    controller: _usernameController,
-                    keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(
-                        hintText: "Enter email", labelText: "Username"),
-                  ),
-                  TextFormField(
-                    controller: _passwordController,
-                    keyboardType: TextInputType.text,
-                    obscureText: true,
-                    decoration: InputDecoration(
-                        hintText: "Enter password", labelText: "Password"),
-                  ),
-                  RaisedButton(
-                    onPressed: () {},
-                    child: Text("Enter"),
-                    color: Colors.orange,
-                    textColor: Colors.white,
-                  )
-                ],
-              ),
+        appBar: AppBar(title: Text("Login page")),
+        // Lets you scroll up and down your app
+        body: Stack(
+          children: <Widget>[
+            Image.asset(
+              "assets/codecode.jpg",
+              fit: BoxFit.cover,
+              color: Colors.black.withOpacity(0.7),
+              colorBlendMode: BlendMode.darken,
             ),
-          ),
-        ),
-      )),
-    );
+            SingleChildScrollView(
+                child: Form(
+              key: formKey,
+              // We want this to be a column, not in rows.
+              child: Card(
+                child: Center(
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      children: <Widget>[
+                        TextFormField(
+                          controller: _usernameController,
+                          keyboardType: TextInputType.emailAddress,
+                          decoration: InputDecoration(
+                              hintText: "Enter email", labelText: "Username"),
+                        ),
+                        SizedBox(
+                          height: 10,
+                        ),
+                        TextFormField(
+                          controller: _passwordController,
+                          keyboardType: TextInputType.text,
+                          obscureText: true,
+                          decoration: InputDecoration(
+                              hintText: "Enter password",
+                              labelText: "Password"),
+                        ),
+                        RaisedButton(
+                          onPressed: () {},
+                          child: Text("Enter"),
+                          color: Colors.orange,
+                          textColor: Colors.white,
+                        )
+                      ],
+                    ),
+                  ),
+                ),
+              ),
+            )),
+          ],
+        ));
   }
 }
